24dba3a7e90e31c03a6f405607c130a2aab5a5d6
root <root@ip-172-31-93-54.ec2.internal>
2019-12-02 06:38:53 +0000

    upgrade to 0.12

--- Modules/DEMOs/terraform-parent-child-demo/child/main.tf
+++ Modules/DEMOs/terraform-parent-child-demo/child/main.tf
@@ -1,5 +1,5 @@
 module "ec2-instance" {
-  source                 = "../EC2"
-   instance_type          = "${var.instance_type_sel["${var.instance_type}"]}"
-
+  source        = "../EC2"
+  instance_type = var.instance_type_sel[var.instance_type]
 }
+
